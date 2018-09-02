d8516e33626e4e62aa9e9690adaaea6c90f42ae0
Cao Liwei <623497281@qq.com>
2018-09-02 11:45:25 +0800

    Chapter6--20180902

---
+++ Chapter6/Practice 6.17.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool HasUppercaseLetter(const string& str)
+{
+	for (auto c : str)
+		if (isupper(c))
+			return true;
+	return false;
+}
+
+void ChangeToLowercaseLetter(string& str)
+{
+	for (auto &c : str)
+		c = tolower(c);
+}
+
+int main()
+{
+	string str = "abcDEf";
+	bool bhasUppercaseLetter = HasUppercaseLetter(str);
+	ChangeToLowercaseLetter(str);
+	cout << "bhasUppercaseLetter = " << bhasUppercaseLetter << endl;
+	cout << "str = " << str << endl;
+
+	bool bhasUppercaseLetter = HasUppercaseLetter("abcDEf");
+	ChangeToLowercaseLetter("abcDEf");
+	cout << "bhasUppercaseLetter = " << bhasUppercaseLetter << endl;
+	cout << "str = " << str << endl;
+
+}
