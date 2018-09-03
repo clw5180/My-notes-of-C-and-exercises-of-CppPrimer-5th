63f40279791d1b9f7df67cb492eb059bca63f648
Cao Liwei <623497281@qq.com>
2018-09-03 17:30:55 +0800

    Chapter7 -- 20180903

---
+++ Chapter7/Practice 7.13.cpp
@@ -0,0 +1,28 @@
+#include "Practice 7.11.h"
+
+int main()
+{
+	//input bookNo¡¢unitsSold and price;
+	CSalesData total(cin); 
+
+	CSalesData trans;
+	if (!total.isbn().empty())
+	{
+		while (read(cin, trans))
+		{
+			if (total.isbn() == trans.isbn())
+				total.combine(trans);
+			else
+			{
+				print(cout, total) << endl;
+				total = trans;
+			}
+		}
+		print(cout, total) << endl;
+	}
+	else
+	{
+		cerr << "No data?!" << endl;
+	}
+	return 0;
+}
