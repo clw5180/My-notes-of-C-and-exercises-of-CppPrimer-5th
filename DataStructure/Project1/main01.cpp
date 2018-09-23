02721cc0e32c2ec5ed2c5e9a70303e1303677b88
Cao Liwei <623497281@qq.com>
2018-09-23 23:58:29 +0800

    add MyList

---
+++ DataStructure/Project1/main01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string.h>
+#include "MyStack.h"
+
+using namespace std;
+
+int main1()
+{
+	//test MyStack
+	int stackSize = 20;
+	MyStack mystack(stackSize);
+	MyStruct data[3];
+	strcpy_s(data[0].szName, "aaa");
+	strcpy_s(data[1].szName, "bbb");
+	strcpy_s(data[2].szName, "ccc");
+
+	//push data into stack
+	cout << "Push data into stack:"<< endl;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << data[i].szName << endl;
+		mystack.push(data[i]);
+	}
+
+	//pop data from stack
+	cout << "Pop data from stack:" << endl;
+	MyStruct tempData;
+	for (int i = 0; i < 3; i++)
+	{
+		mystack.pop(tempData);
+		cout << tempData.szName << endl;
+	}
+
+	return 0;
+}
---
+++ DataStructure/Project1/test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+#include "MyList.h"
+
+int main() 
+{
+	MyList *my = new MyList();
+	my->CreateList(my->GetListHead());
+	printf("before add list\n");
+	my->ShowList(my->GetListHead());
+	my->AddList(5);
+	printf("after add list\n");
+	my->ShowList(my->GetListHead());
+}
